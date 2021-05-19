dc4667d11a4b1e125708de9bc467cb6c0a1fbfe1
HyunnoH <hyunnoh01@gmail.com>
2021-05-20 00:10:00 +0900

    Add login page

--- ui/src/features/login/LoginPage.tsx
+++ ui/src/features/login/LoginPage.tsx
@@ -1,5 +1,47 @@
-// import "./LoginPage.scss";
+import { Button, Form, FormItemProps, Input } from "antd";
+import { Content } from "antd/lib/layout/layout";
+import { useHistory } from "react-router";
+import LoginTitle from "./components/login-title";
+
+import "./LoginPage.scss";
+
+const layout: FormItemProps = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+
+const tailLayout: FormItemProps = {
+  wrapperCol: { offset: 8, span: 16 },
+};
 
 export default function LoginPage() {
-  return <div></div>;
+  const history = useHistory();
+
+  return (
+    <Content className="harbor-login-page">
+      <LoginTitle />
+      <Form onFinish={() => history.push("/")}>
+        <Form.Item label="ID" name="id" className="login-form-item" {...layout}>
+          <Input></Input>
+        </Form.Item>
+        <Form.Item
+          label="Password"
+          name="password"
+          className="login-form-item"
+          {...layout}
+        >
+          <Input.Password></Input.Password>
+        </Form.Item>
+        <Form.Item {...tailLayout}>
+          <Button htmlType="submit" type="primary">
+            Log in
+          </Button>
+        </Form.Item>
+      </Form>
+    </Content>
+  );
 }
---
+++ ui/src/features/login/components/login-title/LoginTitle.tsx
@@ -0,0 +1,10 @@
+import "./LoginTitle.scss";
+import { Typography } from "antd";
+
+export default function LoginTitle() {
+  return (
+    <Typography.Title className="login-title">
+      Log in to Harbor
+    </Typography.Title>
+  );
+}
