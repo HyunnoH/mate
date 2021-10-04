bf3605a051733e20cd5433f4b29755386bec2fe2
HyunnoH <hyunnoh01@gmail.com>
2021-10-04 22:09:15 +0900

    Add basic configuration module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import configuration from "../config/configuration";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+  ],
   controllers: [],
   exports: [],
   providers: [],
